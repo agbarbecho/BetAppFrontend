afb56776f68a1390a2cf61cd11a50daedd89c218
agbarbecho <agbarbecho@sudamericano.edu.ec>
2023-07-11 22:32:16 -0500

    create navigation

---
+++ Navigation.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./assets/screens/Home";
+import CreateProfile from "./assets/screens/CreateProfile";
+import EditProfile from "./assets/screens/EditProfile";
+
