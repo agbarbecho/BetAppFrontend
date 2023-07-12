fa3aa254b303c65579e105c5f0bb935922c733b7
agbarbecho <agbarbecho@sudamericano.edu.ec>
2023-07-12 08:47:45 -0500

    Create Navigator

--- Navigation.js
+++ Navigation.js
@@ -5,3 +5,33 @@ import Home from "./assets/screens/Home";
 import CreateProfile from "./assets/screens/CreateProfile";
 import EditProfile from "./assets/screens/EditProfile";
 
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+    return (
+        <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+                tabBarActiveTintColor: 'purple',
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={Home}
+            />
+            <Tab.Screen name="Profile" component={CreateProfile} />
+
+
+        </Tab.Navigator>
+    );
+}
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <MyTabs />
+
+        </NavigationContainer>
+    )
+}
