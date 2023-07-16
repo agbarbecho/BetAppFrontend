2b3bdc65f39d56200dc6845906b6dd7971ef8fc3
agbarbecho <agbarbecho@sudamericano.edu.ec>
2023-07-16 18:53:42 -0500

    Create  vet user pet

--- Navigation.js
+++ Navigation.js
@@ -4,31 +4,46 @@ import { NavigationContainer } from "@react-navigation/native";
 import User from "./screens/User";
 import Pet from "./screens/Pet";
 import Vet from "./screens/Vet";
-
-
-
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
-    return (
-        <Tab.Navigator
-            initialRouteName="Vet"
-            screenOptions={{
-                tabBarActiveTintColor: 'blue',
-            }}>
-            <Tab.Screen name="Vet" component={Vet} />
-            <Tab.Screen name="User" component={User} />
-            <Tab.Screen name="Pet" component={Pet} />
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator
+      initialRouteName="Vet"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Vet") {
+            iconName = focused ? "md-medical" : "md-medical-outline";
+          } else if (route.name === "User") {
+            iconName = focused ? "person" : "person-outline";
+          } else if (route.name === "Pet") { 
+            iconName = focused ? "paw" : "paw-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#54ccd4',
+        inactiveTintColor: 'gray',
+        showLabel: false,
+      }}
+    >
+      <Tab.Screen name="Vet" component={Vet} />
+      <Tab.Screen name="User" component={User} />
+      <Tab.Screen name="Pet" component={Pet} />
+    </Tab.Navigator>
+  );
 }
 
 export default function Navigation() {
-    return (
-        <NavigationContainer>
-            <MyTabs />
-
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
 }
--- screens/Vet.js
+++ screens/Vet.js
@@ -1,19 +1,51 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from "react-native";
 
 const Vet = () => {
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
 
-    return (
-        <View>
-            <Text
-            style={{
-                fontSize:30,
-                textAlign:"center",
-                marginTop: "20%"
-            }}
-            >Vet</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Vet</Text>
+
+      <TextInput
+        placeholder="Nombre"
+        value={nombre}
+        onChangeText={setNombre}
+        style={styles.input}
+      />
+
+      <TextInput
+        placeholder="Apellido"
+        value={apellido}
+        onChangeText={setApellido}
+        style={styles.input}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+    textAlign: "center",
+    marginBottom: 40,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 20,
+    width: '80%',
+    fontSize: 16,
+  },
+});
 
 export default Vet;
