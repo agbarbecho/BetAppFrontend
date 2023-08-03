6b6bd36a909915c22c4e1d3544704f200e000b31
agbarbecho <agbarbecho@sudamericano.edu.ec>
2023-08-03 17:46:44 -0500

    New Upload

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import Navigation from './Navigation'
-
-
+import React, { useState } from "react";
+import { View } from "react-native";
+import Menu from "./components/Menu";
 
 export default function App() {
+  const [selectedScreen, setSelectedScreen] = useState("Client");
+
   return (
-  <Navigation/>
-   
+    <View style={{ flex: 1 }}>
+      <Menu selectedScreen={selectedScreen} setSelectedScreen={setSelectedScreen} />
+    </View>
   );
 }
